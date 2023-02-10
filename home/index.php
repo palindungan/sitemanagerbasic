0c74b93834c4aacc9b4fc1c60ad5c863ceadfdd5
palindungan <rizkika.palindungan1@gmail.com>
2023-02-10 16:45:42 +0700

    done

--- home/index.php
+++ home/index.php
@@ -18,9 +18,7 @@ require('../admin/common.inc');
 // create root template. notice, returns a reference!!
 $layout1 = $SM_siteManager->rootTemplate("main.cpt");
 
-$intro  = '<B>SiteManager Test Suite</b><br><br>Click on the items in the left to test SiteManager functionality.<br><br>';
-$intro .= 'If all tests do not pass, check your PHP version (Zend PHP 5+ or Roadsend PHP 2.9.3+) and include file setup. If you need more help, head over to the
-           <a href="http://code.roadsend.com" target="_new">Roadsend Community Site</a>.';
+$intro  = '<b>Home</b>';
 
 if (defined('ROADSEND_PCC')) {
     $stats = re_memo_stats();
@@ -32,5 +30,3 @@ $layout1->addText($intro, 'main');
 
 // finish display
 $SM_siteManager->completePage();
-
-?>
